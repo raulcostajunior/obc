d443c9ffe1fbfda09f61489f655a8f79b2fd5854
Raul Costa Junior <raulcostajunior@gmail.com>
2022-12-13 19:54:36 +0100

    Integrated CL11 for command-line parsing. Added .clang-tidy file.

---
+++ src/_version.hpp
@@ -0,0 +1,13 @@
+
+#ifndef __VERSION_HPP
+#define __VERSION_HPP
+
+#include <string>
+
+const int OBOC_VERSION_MAJOR{0};
+const int OBOC_VERSION_MINOR{1};
+const int OBOC_VERSION_PATCH{0};
+
+const std::string OBOC_VERSION_STR{"0.1.0"};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
-#include <iostream>
+/**
+ * oboc - An Oberon-O to WebAssembly compiler.
+ *
+ * The Oberon-O programming language is a subset of the Oberon language defined
+ * in the book, "Compiler Construction" authored by Niklaus Wirth. A revised
+ * version of the book can be downloaded from
+ * https://people.inf.ethz.ch/wirth/CompilerConstruction/CompilerConstruction1.pdf.
+ *
+ */
+#include <string>
 
-int main() {
-    std::cout << "oboc - Oberon-O compiler" << std::endl;
+#include "CLI11.hpp"
+#include "_version.hpp"
+
+// NOLINTBEGIN(bugprone-exception-escape)
+int main(int argc, char **argv) {
+    CLI::App app{"An Oberon-O to WebAssembly compiler"};
+
+    app.set_version_flag("--version", OBOC_VERSION_STR);
+
+    std::string srcFile;
+    CLI::Option *srcFileOption = app.add_option(
+          "src_file", srcFile, "Oberon-O source file to be compiled");
+    srcFileOption->required();
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return app.exit(e);
+    }
 }
+// NOLINTEND(bugprone-exception-escape)
