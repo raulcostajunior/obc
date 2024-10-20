060d674e12dc243935baff125cbf25aeb7732813
Raul Costa Junior <raulcostajunior@gmail.com>
2024-10-20 09:53:24 +0200

    Add compiler class to drive the compilation process.

---
+++ src/Compiler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Raul Costa Junior on 23.07.24.
+//
+
+#include "Compiler.hpp"
---
+++ src/Compiler.hpp
@@ -0,0 +1,22 @@
+#ifndef OBC_COMPILER_HPP
+#define OBC_COMPILER_HPP
+
+#include <vector>
+
+#include "ErrorInfo.hpp"
+
+struct CompilationResults {
+    std::vector<ErrorInfo> errors;
+    // TODO: Add data structure representing the generated WASM module
+};
+
+// TODO: the compiler is the "driver". It should be able to compile from file or from string (
+//       remove the scan from file from the scanner; only the compiler takes care of reading
+//       from the FS.
+class Compiler {
+   public:
+   private:
+};
+
+
+#endif // OBC_COMPILER_HPP
